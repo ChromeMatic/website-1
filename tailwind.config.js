0b46476a2b705b65466c864cf0d746d21cc0f3cd
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2024-06-17 21:26:16 -0500

    changes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
       },  
       backgroundImage:{
         'light_Img':"url('https://images.unsplash.com/photo-1507981919056-e4804b83d74e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-        'dark_Img': "url('/public/images/WhatsApp Image 2024-06-17 at 9.38.45 AM.jpeg')",
+        'dark_Img': "url('/images/WhatsApp Image 2024-06-17 at 9.38.45 AM.jpeg')",
         'goals': "url('https://images.unsplash.com/photo-1544819667-9bfc1de23d4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         'values':"url('https://images.unsplash.com/photo-1605602559567-78f85503acb0?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         'values_light':"url('https://images.unsplash.com/photo-1513530534585-c7b1394c6d51?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
